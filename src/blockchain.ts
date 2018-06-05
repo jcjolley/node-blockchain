8cd330abb93febf7b98a245b635f47f256e161d1
Joshua Jolley <josh@jcjolley.com>
2018-06-04 20:49:37 -0600

    Initial blockchain

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -1,3 +1,48 @@
-export function junk() {
-  console.log(`The project is Alive`);
+import { murmurHash } from 'murmurhash-native';
+
+export class Blockchain {
+  chain: Block[] = [];
+  currentTransactions: Transaction[] = []
+
+  constructor() { }
+
+  createBlock(proof, previousHash = '') {
+    const block = {
+      index: this.chain.length,
+      timestamp: Date.now(),
+      transactions: this.currentTransactions,
+      proof,
+      previousHash: previousHash || this.hash(this.chain[this.chain.length - 1])
+    }
+
+    this.currentTransactions = [];
+    this.chain.push(block);
+  }
+
+  addTransaction(sender, recipient, amount) {
+    this.currentTransactions.push({ sender, recipient, amount });
+    // return a non existent block.
+  }
+
+  hash(block: Block): string {
+    return murmurHash(JSON.stringify(block));
+  }
+
+  get last_block() {
+    return this.chain[this.chain.length - 1];
+  }
+}
+
+export interface Block {
+  index: number;
+  timestamp: number;
+  transactions: Transaction[];
+  proof: number;
+  previousHash: string
+}
+
+export interface Transaction {
+  sender: string;
+  recipient: string;
+  amount: number
 }
